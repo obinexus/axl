add6c1bd7f0a1d6390c29358cf5e4eae20a84e22
OBINexus TDA Agent <tda@obinexus.org>
2025-07-13 16:47:37 +0100

    W

---
+++ include/axl/core/integration/trie_dag.h
@@ -0,0 +1,15 @@
+// include/axl/core/integration/trie_dag.h
+#ifndef AXL_TRIE_DAG_INTEGRATION_H
+#define AXL_TRIE_DAG_INTEGRATION_H
+
+#include <axl/core/trie.h>
+#include <axl/core/dag.h>
+
+// Forward declarations
+struct DAGNode;
+struct TrieNode;
+
+// Minimal placeholder for compilation
+bool execute_axl_with_busting(const char* axl_path, const char* axml_path);
+
+#endif // AXL_TRIE_DAG_INTEGRATION_H
